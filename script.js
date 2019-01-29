a9e2da3fc62b6e565c73f9a19506a06071337d0b
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-01-30 00:35:43 +0100

    Add changes in html, js

--- script.js
+++ script.js
@@ -3,30 +3,15 @@
 
 var elem = document.querySelector('.carousel');
 var flkty = new Flickity( elem, {
-  // options
   cellAlign: 'left',
   contain: true
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.carousel', {
-  // options
-});
-
-var flkty = new Flickity('.carousel');
-
 var previousButton = document.querySelector('.button--previous');
+
 previousButton.addEventListener( 'click', function() {
   flkty.previous();
 });
-
-var previousWrappedButton = document.querySelector('.button--previous-wrapped');
-previousWrappedButton.addEventListener( 'click', function() {
-  flkty.previous( true );
-});
-
-var flkty = new Flickity('.carousel');
 
 var progressBar = document.querySelector('.progress-bar')
 
